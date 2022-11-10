7bb90e828c6bf06c54880d41e517e8581dbb4289
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2022-11-10 20:56:22 +0500

    added methods and tests for complete employee creation form

---
+++ pages/add_employee_page.py
@@ -0,0 +1,116 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class AddEmployee:
+    """
+    Page for adding the employee
+    """
+    # locators
+    link_pim_xpath = "//a[@href='/web/index.php/pim/viewPimModule']//span"
+    link_add_employee_xpath = "//li[@class='oxd-topbar-body-nav-tab']//a[text()='Add Employee']"
+    button_add_employee_xpath = "//div[@class='orangehrm-header-container']//button"
+    input_firstname_xpath = "//input[@name='firstName' and @placeholder='First Name']"
+    input_middlename_xpath = "//input[@name='middleName' and @placeholder='Middle Name']"
+    input_lastname_xpath = "//input[@name='lastName' and @placeholder='Last Name']"
+    input_employee_id_xpath = "//label[text()='Employee Id']/ancestor::div[@class='oxd-input-group oxd-input-field-bottom-space']//input"
+    button_save_xpath = "//div[@class='oxd-form-actions']//button[@type='submit']"
+
+    checkbox_create_login_details_xpath = "//p[text()='Create Login Details']/following-sibling::*[1]//span"
+    input_username_xpath = "//label[text()='Username']/parent::*/following-sibling::*/input"
+    radio_status_enabled_xpath = "//input[@type='radio' and @value='1']/following-sibling::span"
+    radio_status_disabled_xpath = "//input[@type='radio' and @value='2']/following-sibling::span"
+    input_password_xpath = "//label[text()='Password']/parent::*/following-sibling::*/input"
+    input_confirm_password_xpath = "//label[text()='Confirm Password']/parent::*/following-sibling::*/input"
+
+    # action methods for locators
+    def __init__(self, driver):
+        """Initiate webdriver"""
+        self.driver = driver
+        self.wait = WebDriverWait(driver=self.driver, timeout=10, poll_frequency=0.5)
+
+    def click_pim_link(self):
+        self.wait.until(EC.element_to_be_clickable(
+                (By.XPATH, self.link_pim_xpath)
+            ), message='pim link can\'t be clicked'
+        ).click()
+
+    def click_employee_link(self):
+        self.wait.until(EC.element_to_be_clickable(
+                (By.XPATH, self.link_add_employee_xpath)
+            ), message='add employee link can\'t be clicked'
+        ).click()
+
+    def click_add_button(self):
+        self.wait.until(EC.element_to_be_clickable(
+                (By.XPATH, self.button_add_employee_xpath)
+            ), message='add employee button can\'t be clicked'
+        ).click()
+
+    # add employee
+    def write_first_name(self, firstname: str):
+        self.wait.until(EC.visibility_of_element_located(
+                (By.XPATH, self.input_firstname_xpath)
+            ), message='firstname textbox can not be located'
+        ).send_keys(firstname)
+
+    def write_middle_name(self, middlename: str):
+        self.wait.until(EC.visibility_of_element_located(
+                (By.XPATH, self.input_middlename_xpath)
+            ), message='middlename textbox can not be located'
+        ).send_keys(middlename)
+
+    def write_last_name(self, lastname: str):
+        self.wait.until(EC.visibility_of_element_located(
+                (By.XPATH, self.input_lastname_xpath)
+            ), message='lastname textbox can not be located'
+        ).send_keys(lastname)
+
+    def get_employee_id(self):
+        return self.wait.until(EC.visibility_of_element_located(
+                (By.XPATH, self.input_employee_id_xpath)
+            ), message='employee id can not be selected'
+        ).get_attribute('value')
+
+    def click_save_employee(self):
+        self.wait.until(EC.visibility_of_element_located(
+                (By.XPATH, self.button_save_xpath)
+            ), message='save employee button can not be clicked'
+        ).click()
+
+    def click_login_details(self):
+        self.wait.until(EC.element_to_be_clickable(
+                (By.XPATH, self.checkbox_create_login_details_xpath)
+            ), message='login details can not be clicked'
+        ).click()
+
+    def fill_in_username(self, username):
+        self.wait.until(EC.visibility_of_element_located(
+                (By.XPATH, self.input_username_xpath)
+            ), message='username textbox can not be located'
+        ).send_keys(username)
+
+    def click_disabled_button(self):
+        self.wait.until(EC.visibility_of_element_located(
+                (By.XPATH, self.radio_status_disabled_xpath)
+            ), message='disabled button can not be clicked'
+        ).click()
+
+    def click_enabled_button(self):
+        self.wait.until(EC.visibility_of_element_located(
+                (By.XPATH, self.radio_status_enabled_xpath)
+            ), message='enabled button can not be clicked'
+        ).click()
+
+    def fill_in_password(self, password):
+        self.wait.until(EC.visibility_of_element_located(
+                (By.XPATH, self.input_password_xpath)
+            ), message='password textbox can not be located'
+        ).send_keys(password)
+
+    def fill_in_confirm_password(self, password):
+        self.wait.until(EC.visibility_of_element_located(
+                (By.XPATH, self.input_confirm_password_xpath)
+            ), message='confirm password textbox can not be located'
+        ).send_keys(password)
---
+++ tests/test_add_employee.py
@@ -0,0 +1,70 @@
+import random
+import string
+from utilities.custom_logger import LogGenerator
+from utilities.read_properties import ReadConfig
+from pages.login_page import LoginPage
+from pages.add_employee_page import AddEmployee
+
+
+class Test_003_AddEmployee:
+    logger = LogGenerator.generate_log()
+    base_url = ReadConfig.get_app_url()
+    username = ReadConfig.get_username()
+    password = ReadConfig.get_password()
+
+    firstname = ReadConfig.get_user_firstname()
+    middlename = ReadConfig.get_user_middlename()
+    lastname = ReadConfig.get_user_lastname()
+
+    employee_username = 'username1'
+    employee_password = 'password1'
+
+    def generate_password(self, size=8, chars=string.ascii_letters + string.digits + string.punctuation):
+        """Generate random string of give size for password"""
+        return ''.join([random.choice(chars) for x in range(size)])
+
+    # new_employee_password = generate_password(size=16)
+
+    def test_add_employee(self, setup):
+        try:
+            self.logger.info('*** Test_003_AddEmployee ***')
+            self.driver = setup
+            self.driver.get(url=self.base_url)
+            self.driver.maximize_window()
+
+            self.login_page = LoginPage(driver=self.driver)
+            self.logger.info('*** log into admin account ***')
+            self.login_page.set_username(username=self.username)
+            self.login_page.set_password(password=self.password)
+            self.login_page.click_login_button()
+
+            self.add_employee = AddEmployee(driver=self.driver)
+            self.logger.info('*** go to PIM page ***')
+            self.add_employee.click_pim_link()
+
+            self.logger.info('*** go to Add Employee page ***')
+            self.add_employee.click_add_button()
+
+            self.logger.info(f'current url: {self.driver.current_url}')
+            self.logger.info(f'current title: {self.driver.title}')
+
+            # fill employee data
+            self.logger.info('fill user data')
+            self.add_employee.write_first_name(self.firstname)
+            self.add_employee.write_middle_name(self.middlename)
+            self.add_employee.write_last_name(self.lastname)
+            self.emp_id = self.add_employee.get_employee_id()
+
+            self.add_employee.click_login_details()
+            self.add_employee.fill_in_username(self.employee_username)
+            self.add_employee.click_disabled_button()
+            self.add_employee.click_enabled_button()
+            self.add_employee.fill_in_password(self.employee_password)
+            self.add_employee.fill_in_confirm_password(self.employee_password)
+
+            self.add_employee.click_save_employee()
+            self.logger.info(f'data for employee # {self.emp_id} has been saved')
+        finally:
+            self.driver.quit()
+            self.logger.info('*** quit webdriver ***')
+            self.logger.info('*** Test_003_AddEmployee FINISHED ***')
